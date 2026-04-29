54d422bef1c72911d1312e6274ee88a380673a8d
Szymon Mosor BTI2022 <smosor@wsizptibb.local>
2026-04-29 12:23:11 +0200

    Dodanie funkcji doPrzerwy

--- script.js
+++ script.js
@@ -3,25 +3,40 @@ const $$ = e => document.querySelectorAll(e);
 let liczby = $$('.liczba');
 let dwiekropki = $$('.dwiekropki');
 
-function pokazCzas(h, m, s, ms){
-    if(ms < 500){
-        dwiekropki[0].setAttribute('stan','off');
-        dwiekropki[1].setAttribute('stan','off');
-    }else{
-        dwiekropki[0].setAttribute('stan','on');
-        dwiekropki[1].setAttribute('stan','on');
+function pokazCzas(h, m, s) {
+    if (s % 2 === 0) {
+        dwiekropki[0].setAttribute('stan', 'off');
+        dwiekropki[1].setAttribute('stan', 'off');
+    } else {
+        dwiekropki[0].setAttribute('stan', 'on');
+        dwiekropki[1].setAttribute('stan', 'on');
     }
 
-    liczby[0].className = `liczba n${Math.floor(h/10)}`
-    liczby[1].className = `liczba n${h%10}`
-    liczby[2].className = `liczba n${Math.floor(m/10)}`
-    liczby[3].className = `liczba n${m%10}`
-    liczby[4].className = `liczba n${Math.floor(s/10)}`
-    liczby[5].className = `liczba n${s%10}`
+    liczby[0].className = `liczba n${Math.floor(h / 10)}`
+    liczby[1].className = `liczba n${h % 10}`
+    liczby[2].className = `liczba n${Math.floor(m / 10)}`
+    liczby[3].className = `liczba n${m % 10}`
+    liczby[4].className = `liczba n${Math.floor(s / 10)}`
+    liczby[5].className = `liczba n${s % 10}`
 }
 
-function doPrzerwy(h, m, s){
-    
+function doPrzerwy() {
+    let tab = ["07:10", "07:55", "08:00", "08:45", "08:55", "09:40", "09:50", "10:35", "10:45", "11:30",
+        "11:50", "12:35", "12:45", "13:30", "13:40", "14:25", "14:35", "15:20", "15:25", "16:10", "16:15", "17:00"]
+
+    let lastH, lastM;
+    let czas = new Date();
+    let godzina = czas.getHours();
+    let minuta = czas.getMinutes();
+    let czasStr = `${godzina}:${minuta}`;
+
+    tab.forEach(e => {
+        if (e < czasStr) {
+            const [h, m] = e.split(':').map(Number);
+            lastH = h;
+            lastM = m;
+        }
+    })
 }
 
 setInterval(() => {
@@ -29,10 +44,9 @@ setInterval(() => {
     let godzina = String(czas.getHours());
     let minuta = String(czas.getMinutes());
     let sekunda = String(czas.getSeconds());
-    let milisekunda = czas.getMilliseconds();
 
-    pokazCzas(godzina, minuta, sekunda, milisekunda);
-    
+    pokazCzas(godzina, minuta, sekunda);
 
 
-}, 100);
+
+}, 500);
